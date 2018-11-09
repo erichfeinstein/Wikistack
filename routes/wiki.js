cd785a38b0a37c43d1ea5f30ee8871bfb52405d1
Eric Feinstein <erichfeinstein@gmail.com>
2018-11-09 15:05:32 -0500

    working on post wiki page

--- routes/wiki.js
+++ routes/wiki.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 const models = require('../models');
 
-const addPage = require('../views/addPage');
+const {
+  Page
+} = require('../models');
+
+const {
+  addPage
+} = require('../views');
 
 const main = require('../views/main');
 
@@ -13,6 +19,24 @@ router.get('/', async (req, res, next) => {
 });
 
 router.post('/', async (req, res, next) => {
+  const data = res.json(req.body);
+
+  let slug = data.body.title.replace([/^ a - zA - Z\ d\ s/g], '');
+  slug = slug.replace(/ /g, '_');
+
+  console.log(slug);
+
+
+  const page = new Page({
+    title: data.title,
+    content: data.content
+  });
+  try {
+    await page.save();
+    res.redirect('/jhd');
+  } catch (error) {
+    next(error);
+  }
   res.send('got to POST /wiki/');
 })
 
--- views/addPage.js
+++ views/addPage.js
@@ -1,38 +1,45 @@
 const html = require("html-template-tag");
 const layout = require("./layout");
 
-module.exports = () => layout(html`
-  <h3>Add a Page</h3>
-  <hr>
-  <form method="POST" action="/wiki/">
-    
-    <div>
-    <label for="name">Name:</label><input name="name" type="text" placeholder="Enter name">
-    </div><br>
-    
-    <div>
-    <label for="email">Email:</label><input name="email" type="email">
-    </div><br>
-    
-    <div class="form-group">
-      <label for="title" class="col-sm-2 control-label">Page Title:</label>
-      <div class="col-sm-10">
-        <input id="title" name="title" type="text" class="form-control"/>
-      </div>
-    </div><br>
+module.exports = () => layout(html `
+  <head>
+    <link href="/stylesheets/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <h3>
+      Add a Page
+    </h3>
+    <hr>
+    <form method="POST" action="/wiki/">
+      
+      <div>
+      <label for="name">Name:</label><input name="name" type="text" placeholder="Enter name">
+      </div><br>
+      
+      <div>
+      <label for="email">Email:</label><input name="email" type="email">
+      </div><br>
+      
+      <div class="form-group">
+        <label for="title" class="col-sm-2 control-label">Page Title:</label>
+        <div class="col-sm-10">
+          <input id="title" name="title" type="text" class="form-control"/>
+        </div>
+      </div><br>
 
-    <div>
-    <label for="content">Article content:</label>
-    <input name="content" type="text" placeholder="Enter content" class="form-control">
-    </div><br>
-    
-    <div>
-    <input name="status" type="text">
-    </div><br>
+      <div>
+      <label for="content">Article content:</label>
+      <input name="content" type="textarea" placeholder="Enter content" class="form-control">
+      </div><br>
+      
+      <div>
+      <input name="status" type="text">
+      </div><br>
 
-    <div class="col-sm-offset-2 col-sm-10">
-      <button type="submit" class="btn btn-primary">submit</button>
-    </div><br>
-  
-  </form>
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-primary">submit</button>
+      </div><br>
+    
+    </form>
+  </body>
 `);
